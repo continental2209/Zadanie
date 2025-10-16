77da1614c915dfe4706e5ba0a06d019978b7f146
ЯкимовАртур <email@example.com>
2025-10-16 16:00:17 +0500

    Initial commit: project structure with gitignore

---
+++ src/main.py
@@ -0,0 +1,9 @@
+print("Практическая работа 12")
+print("Это настройка системы контроля версий")
+
+def calculate_sum(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    result = calculate_sum(5, 3)
+    print(f"5 + 3 = {result}")
